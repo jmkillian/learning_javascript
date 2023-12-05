b3b5c3f167e7cd65686d4a6808dd30d65a02121a
jmkillian <jkchem9120@gmail.com>
2023-12-05 12:39:04 -0800

    Added variables.js file

---
+++ variables.js
@@ -0,0 +1,19 @@
+// NUMBERS
+
+let x = 5
+console.log(x)
+
+let y = 5.5
+console.log(y)
+
+// STRINGS
+
+let z = "Hello"
+console.log(z)
+
+let a = "A String can be a sentence."
+console.log(a)
+
+// BOOLEAN
+let b = true
+console.log(b)
