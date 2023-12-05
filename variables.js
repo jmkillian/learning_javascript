90db31767c1f8a2b88e625027087ebc5052e5fc5
jmkillian <jkchem9120@gmail.com>
2023-12-05 12:49:04 -0800

    Added variables.js file with operations on variables

--- variables.js
+++ variables.js
@@ -17,3 +17,33 @@ console.log(a)
 // BOOLEAN
 let b = true
 console.log(b)
+
+// Doing math - reassigning variables
+let c = 3
+console.log(c)
+c = 4 // reassign it
+console.log(c)
+
+// CONSTANTS
+const d = 5 // you can't reassign it
+
+// OPERATIONS with numbers
+let e = 3
+let f = 4
+let g = e + f
+console.log(f)
+
+let h = e - f
+console.log(h)
+
+let i = e*f
+console.log(i)
+
+let j =e/f
+console.log(j)
+
+let k = f%e // MODULUS
+console.log(k)
+
+e = e + 1
+console.log(e)
