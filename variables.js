fe9f6b349da3d3df80975ed727ef5e94df6ca7b2
jmkillian <jkchem9120@gmail.com>
2023-12-11 14:13:58 -0800

    Learned about concatenating

--- variables.js
+++ variables.js
@@ -47,3 +47,17 @@ console.log(k)
 
 e = e + 1
 console.log(e)
+
+// Concatenating strings
+let firstWord = "Hello"
+let secondWord = "World!"
+let sentence = firstWord + secondWord
+console.log(sentence)
+sentence = firstWord + " " + secondWord
+console.log(sentence)
+
+console.log("The value of x is " + x)
+console.log("The value of a is " + a)
+
+console.log("The value of e + f is " + (e = f ))
+
