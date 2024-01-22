988cd943ad9d97817eb71abe1ebbd3aa7a3df4c2
jmkillian <jkchem9120@gmail.com>
2024-01-22 14:38:20 -0800

    Adding new js that I learned - objects

--- array.js
+++ array.js
@@ -13,3 +13,25 @@ console.log(fruits)
 
 fruits.splice(1, 0, 'banana')  // Adds to array - position, number to delete, what to add
 console.log(fruits)
+
+// Create cat object with array
+let cats = [
+    {
+        'name': 'Fluffy',
+        'age': 2
+    },
+    {
+        'name': 'Whiskers',
+        'age': 10
+    },
+    {
+        'name': 'Munchkin',
+        'age': 5
+    }
+]
+
+console.log(cats)
+
+console.log(cats[0])
+
+console.log(cats[1].age)
---
+++ objects.js
@@ -0,0 +1,20 @@
+// name value pairs object
+let pet = {
+    'name': 'Fluffy',
+    'type': 'cat',
+    'weight': 12,
+    'age': '5'
+}
+
+console.log(pet)
+console.log(pet.type)
+
+// objects can be in arrays
+let dog = {
+    'name': 'Fido',
+    'toys': ['Squeaky pretzel', 'Rubber Ball', 'Chewy Bone']
+}
+
+console.log(dog)
+
+console.log(dog.toys[1])
